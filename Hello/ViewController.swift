653f6336c6c3ef427582e2fe0cb4132d248ba5ec
Гордей Гусельников <guselnikovgordey@icloud.com>
2020-05-28 21:51:07 +0700

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Гордей Гусельников on 28.05.2020.
+//  Copyright © 2020 gordeyStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
