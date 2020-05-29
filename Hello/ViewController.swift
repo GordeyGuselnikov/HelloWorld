ecdfc73e3392553b5955b6d41da1e5c78d434394
Гордей Гусельников <guselnikovgordey@icloud.com>
2020-05-29 16:55:44 +0700

    commit

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    @IBOutlet weak var showTextButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        helloWorldLabel.isHidden = true
+        helloWorldLabel.textColor = .brown
+        showTextButton.layer.cornerRadius = 10
     }
+    
+    @IBAction func showTextButtonPressed(_ sender: UIButton) {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            showTextButton.setTitle("Скрыть", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            showTextButton.setTitle("Показать", for: .normal)
+        }
+    }
+    
 
     
 }
